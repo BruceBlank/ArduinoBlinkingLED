9834a92294b6558c58292c55f3c9368f9d3e0285
Bruce Blank <Torsten.Blank@gmail.com>
2014-05-17 16:10:34 +0200

    C++ class implemented timeScale will be set on every delay

--- ArduinoBlinkingLED.cpp
+++ ArduinoBlinkingLED.cpp
@@ -1,60 +1,96 @@
 // Do not remove the include below
 #include <Arduino.h>
 
-int led = 13;
-unsigned long timeScale=0;
+//TODO: implement the button to switch between SOS- and regular blinking
 
-void initialization(void)
+class CBlinkingLED {
+public:
+	// the constructor
+	CBlinkingLED(uint8_t ledpin, uint8_t potpin, uint8_t buttonpin);
+	// does the work once
+	void doit();
+private:
+	const uint8_t m_ledpin;
+	const uint8_t m_potpin;
+	const uint8_t m_buttonpin;
+	unsigned long m_timeScale;
+
+	// the short signal (dit) and the gap
+	void dit();
+	// the long signal (dah) and the gap
+	void dah();
+	// (additional) silence between two letters
+	void letterGap();
+	// (additional) silence between two words
+	void wordGap();
+	// a special internalDelay method set timeScale on every delay
+	// time is a generalized time value
+	void internalDelay(float time);
+};
+
+void CBlinkingLED::internalDelay(float time)
 {
-	init();
-	pinMode(led, OUTPUT);
+	// read and set timeScale from A2: [0,1023] -> [25,800]
+	int sensorValue = analogRead(m_potpin);
+	m_timeScale = (unsigned long)(sensorValue * (775.0 / 1023) + 25);
+
+	//TODO: here, an event handler for the button should be implemented
+	delay(m_timeScale * time);
 }
 
-void dit(void)
+
+CBlinkingLED::CBlinkingLED(uint8_t ledpin, uint8_t potpin, uint8_t buttonpin) :
+				m_ledpin(ledpin) , m_potpin(potpin), m_buttonpin(buttonpin) , m_timeScale(0)
 {
-// the short signal (dit) and the gap
-	digitalWrite(led, HIGH);
-	delay(timeScale * 1);
-	digitalWrite(led, LOW);
-	delay(timeScale * 1);
+	pinMode(m_ledpin, OUTPUT);
+	pinMode(m_buttonpin, INPUT);
+	// no pinMode for analog input necessary
 }
 
-void dah(void)
+void CBlinkingLED::dit(void)
 {
-// the long signal (dah) and the gap
-	digitalWrite(led, HIGH);
-	delay(timeScale * 3);
-	digitalWrite(led, LOW);
-	delay(timeScale * 1);
+	digitalWrite(m_ledpin, HIGH);
+	internalDelay(1);
+	digitalWrite(m_ledpin, LOW);
+	internalDelay(1);
 }
 
-void letterGap(void)
+void CBlinkingLED::dah(void)
 {
-// (additional) silence between two letters
-	delay(timeScale * 2);
+	digitalWrite(m_ledpin, HIGH);
+	internalDelay(3);
+	digitalWrite(m_ledpin, LOW);
+	internalDelay(1);
 }
 
-void wordGap(void)
+void CBlinkingLED::letterGap(void)
 {
-	// (additional) silence between two words	  while(1);
-	delay(timeScale * 4);
+	internalDelay(2);
+}
+
+void CBlinkingLED::wordGap(void)
+{
+	internalDelay(4);
+}
+
+void CBlinkingLED::doit()
+{
+	// send SOS
+	dit(); dit(); dit(); letterGap();
+	dah(); dah(); dah(); letterGap();
+	dit(); dit(); dit(); letterGap(); wordGap();
 }
 
 int main(void)
 {
-  initialization();
+	// general initialization
+	init();
 
-  while (1)
-  {
-	  // read and set timeScale from A2: [0,1023] -> [100,500]
-	  int sensorValue = analogRead(A2);
-	  timeScale = (unsigned long)(sensorValue * (400.0 / 1023) + 100);
-	  //Serial.println(timeScale);
+	CBlinkingLED blinkLED(13, A0, 8);
 
-	  // send SOS
-	  dit(); dit(); dit(); letterGap();
-	  dah(); dah(); dah(); letterGap();
-	  dit(); dit(); dit(); letterGap(); wordGap();
-  }
-  return 0; // never reached
+	while (true)
+	{
+		blinkLED.doit();
+	}
+	return 0; // never reached
 }
